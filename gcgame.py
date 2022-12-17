0efbeb29e8207c11c2a361045819300b7658716d
vishalbhoir18 <67859075+vishalbhoir18@users.noreply.github.com>
2022-12-17 09:04:31 +0530

    pilot code added

---
+++ gcgame.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+st.title("Genetic Code Game")
+
+# Define the genetic code
+genetic_code = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+}
+
+# Get a random codon from the genetic code
+codon = random.choice(list(genetic_code.keys()))
+
+# Prompt the user to guess the amino acid for the codon
+amino_acid = st.text_input("What is the amino acid for the codon "+codon+"?")
+
+# Check the user's answer
+if amino_acid.upper() == genetic_code[codon]:
+    st.write("Correct! The amino acid for "+codon+" is "+genetic_code[codon]+".")
+else:
+    st.write("Incorrect. The amino acid for "+codon+" is "+genetic_code[codon]+".")
